06bfd2606058a2973a87d486a4945029a5edcd3a
Kudret Ünal <kudret@MacBook-Pro-von-Kudret.local>
2022-11-08 21:50:26 +0100

    Test Hat leider Probleme :(

--- src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
+++ src/main/kotlin/com/egotec/starterproject/TodoAPI.kt
@@ -12,24 +12,54 @@ class TodoAPI {
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     fun getTodo(@PathParam("id") id: String): TodoEntity {
-        val state = ThreadState.begin()
-        return state.em.find(TodoEntity::class.java, id) ?: throw WebApplicationException(Response.Status.NOT_FOUND)
+        val ts = ThreadState.begin()
+        return ts.em.find(TodoEntity::class.java, id) ?: throw WebApplicationException(Response.Status.NOT_FOUND)
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getAllTodos(): List<TodoEntity> {
+        val ts = ThreadState.begin();
+        return ts.em.createQuery("SELECT todo FROM TodoEntity todo", TodoEntity::class.java).resultList
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    fun updateTodoById(@PathParam("id") id: String, newTodo: TodoEntity): TodoEntity {
+        val ts = ThreadState.begin()
+        val e = ts.em.find(TodoEntity::class.java, id);
+        e.content = newTodo.content
+        e.done = newTodo.done
+        ts.em.transaction.begin();
+        ts.em.merge(e)
+        ts.em.transaction.commit();
+        return e
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     fun createTodo(todoEntity: TodoEntity): TodoEntity {
-        val state = ThreadState.begin()
-        state.em.transaction.begin()
-        state.em.merge(todoEntity)
-        state.em.transaction.commit()
+        val ts = ThreadState.begin()
+        ts.em.transaction.begin()
+        ts.em.merge(todoEntity)
+        ts.em.transaction.commit()
         return todoEntity
     }
 
-    // TODO:
-    // getAllTodos
-    // deleteTodo
-    // updateTodo
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    fun deleteTodoById(@PathParam("id") id: String): TodoEntity {
+        val ts = ThreadState.begin()
+        val e = ts.em.find(TodoEntity::class.java, id);
+        ts.em.transaction.begin();
+        ts.em.remove(e)
+        ts.em.transaction.commit();
+        return e
+    }
 
 }
